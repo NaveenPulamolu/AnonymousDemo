d9f06dfa624d8bbdf785ebeee8f6a268718e7d74
NaveenPulamolu <naveenp992@gmail.com>
2016-12-28 21:02:06 -0600

    Add files via upload

---
+++ Anonymous.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullablesNAnonymousDemo
+{
+    class Anonymous
+    {
+       public void details()
+        {
+            try
+            {
+                var v = new { name = "Naveen", Number = 123, money = 45.67 };
+                Console.WriteLine(v.name + "-" + v.Number + "-" + v.money);
+                // The type of the variable is decided after it is assigned a value
+                int a = 0;
+                int b = 10;
+                int c = b / a;
+                Console.WriteLine(c);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullablesNAnonymousDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a = null; It is not possible to assign null values to variables this can be acheived by using nullable types in C#
+            int? a = null; // assigning a null value to the variable type Int.
+            if (a.HasValue)
+            {
+                Console.WriteLine("a contains some values in it");
+            }
+            else
+            {
+                Console.WriteLine("a Containes null value in it");
+            }
+            Anonymous obj = new Anonymous();
+            obj.details();
+            Console.ReadLine();
+            // the use of nullables types is more when reading values from database which contains null values in the table.
+
+        }
+    }
+}
